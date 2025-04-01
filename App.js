aeb1703d5c7755e831554bf6bca6c4c6478db139
EvelynOliveira1200 <evelynoliveira.tds1.senai@gmail.com>
2025-04-01 14:20:19 -0300

    feat: update img header

--- App.js
+++ App.js
@@ -15,11 +15,9 @@ export default function App() {
         <Stack.Screen name="SignIn" component={SignInScreen} options={{
           headerTitle: () =>
             <Image
-              source={require("./img/logoimg.png")}
-              style={{ width: 100, height: 90, marginRight: 70 }}
+              source={require("./img/logobranco.png")}
+              style={{ width: 100, height: 50, marginRight: 70 }}
             />,
-          headerStyle: { backgroundColor: 'black' },
-          headerTintColor: 'white'
         }} />
       </Stack.Navigator>
     </NavigationContainer>
